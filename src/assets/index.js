72a244b6e039bbef9ac0971b5a3ef8306ec65be3
Devanshu Gautam <devgauta@publicisgroupe.net>
2023-04-18 09:35:35 +0530

    my portfolio

---
+++ src/assets/index.js
@@ -0,0 +1,77 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import node from "./tech/nodejs.png";
+import nodejs from "./tech/node.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import postgres from "./tech/postgres.png";
+import mssql from "./tech/mssql.png";
+import dart from "./tech/dart.png";
+import nextjs from "./tech/nextjs.png";
+import flutter from "./tech/flutter.jpg";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import imgGlobalInfotech from "./company/img.jpg";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import youngminds from "./project/youngmind.jfif";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  node,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  imgGlobalInfotech,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  youngminds,
+  postgres,
+  mssql,
+  dart,
+  nextjs,
+  flutter
+};
